cdadcd1bc94c1af09e6f2ddbe7dba270f8df75f8
ccastano46 <castanocamilo522@gmail.com>
2024-09-13 21:25:44 -0500

    Aplicando SpringBoot parte 2

--- src/main/java/main/BlueprintsApp.java
+++ src/main/java/main/BlueprintsApp.java
@@ -1,8 +1,22 @@
 package main;
 
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class BlueprintsApp {
 
+    private final BlueprintsServices blueprintsServices;
+
+    @Autowired
+    public BlueprintsApp(BlueprintsServices blueprintsServices) {
+        this.blueprintsServices = blueprintsServices;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(BlueprintsApp.class, args);
+    }
 }
